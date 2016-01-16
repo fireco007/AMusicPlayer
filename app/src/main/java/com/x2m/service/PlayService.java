9b9b94ac0fdbda9b87c3da16f25edf7448e015d6
xieming <c04n05@gmail.com>
2016-01-16 14:45:14 +0800

    修复mediaplay的bug

--- app/src/main/java/com/x2m/service/PlayService.java
+++ app/src/main/java/com/x2m/service/PlayService.java
@@ -13,14 +13,13 @@ import java.io.IOException;
 /**
  * Created by Administrator on 2016/1/13.
  */
-public class PlayService extends Service{
+public class PlayService extends Service {
 
     private static String TAG = "MyPlayService";
 
-    private MediaPlayer mediaPlayer = new MediaPlayer();
+    private MediaPlayer mediaPlayer;
 
-    class PlayerBinder extends IPlayerAidlInf.Stub
-    {
+    class PlayerBinder extends IPlayerAidlInf.Stub {
 
         @Override
         public void play(String filePath) throws RemoteException {
@@ -54,21 +53,44 @@ public class PlayService extends Service{
     public void onCreate() {
         Log.i(TAG, "create service");
         super.onCreate();
+
+        initPlay();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-
+        Log.i(TAG, "destroy service");
         if (mediaPlayer != null) {
             mediaPlayer.stop();
             mediaPlayer.release();
         }
     }
 
+    private void initPlay() {
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setOnCompletionListener(
+                new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        Log.i(TAG, "play complete");
+
+                        //默认播放完成会切换到stop状态
+                        //在stop状态下不能setDataSource
+                        //详细请参看mediaplayer生命周期
+                        mp.reset();
+                    }
+                });
+    }
 
     public void startPlay(String filePath) {
         try {
+
+            //如果正在播放音乐，需要先重置
+            if (mediaPlayer.isPlaying()) {
+                mediaPlayer.reset();
+            }
+
             mediaPlayer.setDataSource(filePath);
             mediaPlayer.prepare();
             mediaPlayer.start();
@@ -79,6 +101,7 @@ public class PlayService extends Service{
     }
 
     public void stopPlay() {
-        mediaPlayer.stop();
+        if (mediaPlayer.isPlaying())
+            mediaPlayer.stop();
     }
 }
